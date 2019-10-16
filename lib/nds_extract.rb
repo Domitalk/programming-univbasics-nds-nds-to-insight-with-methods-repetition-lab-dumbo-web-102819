048c8eac28516705d719d93681b810aee4e7bc0f
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-16 18:26:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,12 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  list_d = Array.new
+  dir_index = 0
+  while dir_index < source.size do
+    list_d << source[dir_index]
+    dir_index += 1
+  end
 end
 
 def total_gross(source)
@@ -39,5 +45,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
